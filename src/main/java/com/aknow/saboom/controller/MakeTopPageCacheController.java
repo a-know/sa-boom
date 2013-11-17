07c64930824f2b4a86b24a8b91b68a67e09d8c9f
a-know <a.know.3373@gmail.com>
2013-11-17 09:55:44 +0900

    トップページ用のcacheをmemcacheを用いるのではなくdatastoreに登録する形にした refs #3

--- src/main/java/com/aknow/saboom/controller/MakeTopPageCacheController.java
+++ src/main/java/com/aknow/saboom/controller/MakeTopPageCacheController.java
@@ -8,103 +8,103 @@ import java.util.TimeZone;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 import org.slim3.datastore.Datastore;
-import org.slim3.memcache.Memcache;
 
 import com.aknow.saboom.meta.TotalPlayCountByArtistMeta;
+import com.aknow.saboom.model.RankingDataForTopPage;
 import com.aknow.saboom.model.TotalPlayCountByArtist;
-import com.aknow.saboom.util.Consts;
 import com.aknow.saboom.util.DatastoreCacheUtility;
 import com.google.appengine.api.datastore.Key;
 
 public class MakeTopPageCacheController extends Controller {
-	
-    private static Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
 
-    @Override
-    public Navigation run() throws Exception {
-    	
+	private static Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
 
-        String current_year = Integer.valueOf(calendar.get(Calendar.YEAR)).toString();
-        String current_month = Integer.valueOf(calendar.get(Calendar.MONTH) + 1).toString();
+	@Override
+	public Navigation run() throws Exception {
 
-        //差分元となる前月情報を特定
-        String pre_year = null;
-        String pre_month = Integer.valueOf(calendar.get(Calendar.MONTH)).toString();
-        if("0".equals(pre_month)){
-            pre_year = Integer.valueOf(calendar.get(Calendar.YEAR) - 1).toString();
-            pre_month = "12";
-        }else{
-            pre_year = Integer.valueOf(calendar.get(Calendar.YEAR)).toString();
-        }
-        
-        getRankingDataByArtist(current_year, current_month, pre_year, pre_month);
+		String current_year = Integer.valueOf(calendar.get(Calendar.YEAR)).toString();
+		String current_month = Integer.valueOf(calendar.get(Calendar.MONTH) + 1).toString();
 
-        return null;
-    }
+		//差分元となる前月情報を特定
+		String pre_year = null;
+		String pre_month = Integer.valueOf(calendar.get(Calendar.MONTH)).toString();
+		if("0".equals(pre_month)){
+			pre_year = Integer.valueOf(calendar.get(Calendar.YEAR) - 1).toString();
+			pre_month = "12";
+		}else{
+			pre_year = Integer.valueOf(calendar.get(Calendar.YEAR)).toString();
+		}
 
-    private void getRankingDataByArtist(String saki_year, String saki_month, String moto_year, String moto_month){
+		RankingDataForTopPage e = getRankingDataByArtist(current_year, current_month, pre_year, pre_month);
 
-    	List<TotalPlayCountByArtist> sorted_list = Memcache.get(Consts.SabunArtistList_KEY + saki_year + saki_month + moto_year + moto_month);
-    	
-    	if(sorted_list == null){
-    		TotalPlayCountByArtistMeta meta = new TotalPlayCountByArtistMeta();
-    		List<Key> sakiKeyList = Datastore.query(meta).filter(meta.year.equal(saki_year), meta.month.equal(saki_month)).asKeyList();
-    		List<TotalPlayCountByArtist> saki_list = DatastoreCacheUtility.get(new TotalPlayCountByArtist(), sakiKeyList);
-    		List<Key> motoKeyList = Datastore.query(meta).filter(meta.year.equal(moto_year), meta.month.equal(moto_month)).asKeyList();
-    		List<TotalPlayCountByArtist> moto_list = DatastoreCacheUtility.get(new TotalPlayCountByArtist(), motoKeyList);
-//    		List<TotalPlayCountByArtist> saki_list = Datastore.query(meta).filter(meta.year.equal(saki_year), meta.month.equal(saki_month)).asList();
-//    		List<TotalPlayCountByArtist> moto_list = Datastore.query(meta).filter(meta.year.equal(moto_year), meta.month.equal(moto_month)).asList();
+		// put cache entity
+		DatastoreCacheUtility.put(e, e.getKey());
 
-    		List<String> saki_artistList = new ArrayList<String>();
-    		List<String> moto_artistList = new ArrayList<String>();
+		return null;
+	}
 
-    		for(TotalPlayCountByArtist e1 : saki_list){
-    			saki_artistList.add(e1.getArtistName());
-    		}
-    		for(TotalPlayCountByArtist e2 : moto_list){
-    			moto_artistList.add(e2.getArtistName());
-    		}
+	private RankingDataForTopPage getRankingDataByArtist(String saki_year, String saki_month, String moto_year, String moto_month){
 
-    		//差分取得
-    		List<TotalPlayCountByArtist> sabun_list = new ArrayList<TotalPlayCountByArtist>();
+		List<TotalPlayCountByArtist> sorted_list = new ArrayList<TotalPlayCountByArtist>();
 
-    		for(TotalPlayCountByArtist e1 : saki_list){
-    			TotalPlayCountByArtist tempElement = new TotalPlayCountByArtist();
+		TotalPlayCountByArtistMeta meta = new TotalPlayCountByArtistMeta();
+		List<Key> sakiKeyList = Datastore.query(meta).filter(meta.year.equal(saki_year), meta.month.equal(saki_month)).asKeyList();
+		List<TotalPlayCountByArtist> saki_list = DatastoreCacheUtility.get(new TotalPlayCountByArtist(), sakiKeyList);
+		List<Key> motoKeyList = Datastore.query(meta).filter(meta.year.equal(moto_year), meta.month.equal(moto_month)).asKeyList();
+		List<TotalPlayCountByArtist> moto_list = DatastoreCacheUtility.get(new TotalPlayCountByArtist(), motoKeyList);
 
-    			if(moto_artistList.contains(e1.getArtistName())){
-    				TotalPlayCountByArtist e2 = moto_list.get(moto_artistList.indexOf(e1.getArtistName()));
-    				tempElement.setArtistName(e1.getArtistName());
-    				tempElement.setYear(saki_year);
-    				tempElement.setMonth(saki_month);
-    				tempElement.setTotalPlayCount(Integer.valueOf(e1.getTotalPlayCount().intValue() - e2.getTotalPlayCount().intValue()));
-    				sabun_list.add(tempElement);
-    			}
-    		}
+		List<String> saki_artistList = new ArrayList<String>();
+		List<String> moto_artistList = new ArrayList<String>();
 
-    		//再生回数でソート
-    		sorted_list = new ArrayList<TotalPlayCountByArtist>();
+		for(TotalPlayCountByArtist e1 : saki_list){
+			saki_artistList.add(e1.getArtistName());
+		}
+		for(TotalPlayCountByArtist e2 : moto_list){
+			moto_artistList.add(e2.getArtistName());
+		}
 
-    		while(sorted_list.size() < 10){
+		//差分取得
+		List<TotalPlayCountByArtist> sabun_list = new ArrayList<TotalPlayCountByArtist>();
 
-    			int max = 0;
-    			int maxIndex = 0;
-    			TotalPlayCountByArtist tempElement = null;
+		for(TotalPlayCountByArtist e1 : saki_list){
+			TotalPlayCountByArtist tempElement = new TotalPlayCountByArtist();
 
-    			for(int i = 0; i < sabun_list.size(); i++){
-    				TotalPlayCountByArtist e = sabun_list.get(i);
-    				if(e.getTotalPlayCount().intValue() > max){
-    					tempElement = new TotalPlayCountByArtist();
-    					tempElement.setArtistName(e.getArtistName());
-    					tempElement.setTotalPlayCount(e.getTotalPlayCount());
-    					max = e.getTotalPlayCount().intValue();
-    					maxIndex = i;
-    				}
-    			}
+			if(moto_artistList.contains(e1.getArtistName())){
+				TotalPlayCountByArtist e2 = moto_list.get(moto_artistList.indexOf(e1.getArtistName()));
+				tempElement.setArtistName(e1.getArtistName());
+				tempElement.setYear(saki_year);
+				tempElement.setMonth(saki_month);
+				tempElement.setTotalPlayCount(Integer.valueOf(e1.getTotalPlayCount().intValue() - e2.getTotalPlayCount().intValue()));
+				sabun_list.add(tempElement);
+			}
+		}
+		//再生回数でソート
+		while(sorted_list.size() < 10){
 
-    			sorted_list.add(tempElement);
-    			sabun_list.remove(maxIndex);
-    		}
-    		Memcache.put(Consts.SabunArtistList_KEY + saki_year + saki_month + moto_year + moto_month, sorted_list);
-    	}
-    }
+			int max = 0;
+			int maxIndex = 0;
+			TotalPlayCountByArtist tempElement = null;
+
+			for(int i = 0; i < sabun_list.size(); i++){
+				TotalPlayCountByArtist e = sabun_list.get(i);
+				if(e.getTotalPlayCount().intValue() > max){
+					tempElement = new TotalPlayCountByArtist();
+					tempElement.setArtistName(e.getArtistName());
+					tempElement.setTotalPlayCount(e.getTotalPlayCount());
+					max = e.getTotalPlayCount().intValue();
+					maxIndex = i;
+				}
+			}
+
+			sorted_list.add(tempElement);
+			sabun_list.remove(maxIndex);
+		}
+		
+		// cache entity
+		RankingDataForTopPage e = new RankingDataForTopPage();
+		e.setKey(RankingDataForTopPage.createKey(moto_year, moto_month, saki_year, saki_month));
+		e.setSortedList(sorted_list);
+		
+		return e;
+	}
 }
